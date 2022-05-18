9b7bb24730505d63035a95ab1fd873840aa8bee2
prostoleo <73364400+prostoleo@users.noreply.github.com>
2022-05-18 14:48:31 +0300

    feat: adв transition to Card

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,10 +1,12 @@
 <script>
+	import { fade } from 'svelte/transition';
 	export let pokeman;
 </script>
 
 <a
 	href={`/pokemon/${pokeman.id}`}
 	class="shadow shadow-md p-4 flex flex-col items-center hover:(shadow-lg)"
+	transition:fade
 >
 	<img class="h-40 w-40 object-cover" src={pokeman.image} alt={pokeman.name} />
 	<h2 class="text-xl font-semibold capitalize">{pokeman.name}</h2>
